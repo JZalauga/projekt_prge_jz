22bfc80ab730be72146bb9b8534150c63c3b40f2
Marek Wyszyński <45630165+gitgeoman@users.noreply.github.com>
2026-01-11 22:15:43 +0100

    next step dodaje cors

--- config/fastapi/app/main.py
+++ config/fastapi/app/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 from app.routers.static_content import router
 from app.routers.db_insert import router_insert
+
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI(title="Mapbook API")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # W środowisku deweloperskim pozwalamy na wszystko
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router, prefix="/app")
 app.include_router(router_insert, prefix="/app")
