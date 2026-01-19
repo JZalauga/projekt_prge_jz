ddb1ec771b6801d7680794e625003db96e324d47
kubaz <kuba.zaluga@gmail.com>
2026-01-19 01:32:00 +0100

    feat: database getter for workers

--- config/fastapi/app/routers/dynamic_content.py
+++ config/fastapi/app/routers/dynamic_content.py
@@ -4,6 +4,9 @@ from app.settings import db_name, db_user, db_password
 
 router_get_cemetery = APIRouter()
 
+router_get_worker = APIRouter()
+
+
 
 def connect_to_db(db_name: str, db_user: str, db_password: str):
     return create_engine(
@@ -27,4 +30,23 @@ async def get_users():
 
     except Exception as e:
         print(f' bląd podczas get_users')
+        return {"status": 'error'}
+
+
+@router_get_worker.get("/get_worker")
+async def get_worker():
+    try:
+        db_connection = connect_to_db(db_name=db_name, db_user=db_user, db_password=db_password)
+
+        sql = text("""select *
+                    from workers;""")
+        
+        with db_connection.connect() as conn:
+            result = conn.execute(sql)
+            users = [dict(row._mapping) for row in result]
+
+        return {"status": "success", "data": users}
+
+    except Exception as e:
+        print(f' bląd podczas get_worker')
         return {"status": 'error'}
