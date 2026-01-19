f4d312a9917e69a7fc7b2a4ae40bdce614fe9fd5
kubaz <kuba.zaluga@gmail.com>
2026-01-19 01:11:47 +0100

    feat: added cemetery insert to database module

--- config/fastapi/app/routers/db_insert.py
+++ config/fastapi/app/routers/db_insert.py
@@ -12,27 +12,46 @@ def connect_to_db(db_name: str, db_user: str, db_password: str):
         f"postgresql://{db_user}:{db_password}@postgis:5432/{db_name}"
     )
 
+def get_coord_osm(location: str) -> list[float]:
+        import requests
+        url = "https://nominatim.openstreetmap.org/search"
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                          'AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/130.0 Safari/537.36'
+        }
+        params = {
+            'q': location,
+            'format': 'json'
+        }
+        response = requests.get(url, params=params, headers=headers)
+        data = response.json()
+        latitude = float(data[0]['lat'])
+        longitude = float(data[0]['lon'])
+        return [latitude, longitude]
 
 class UserData(BaseModel):
     name: str
-    posts: int
     location: str
 
 
-@router_insert.post("/insert_user")
+@router_insert.post("/insert_cemetery")
 async def insert_user(user: UserData):
     try:
         db_connection = connect_to_db(db_name=db_name, db_user=db_user, db_password=db_password)
 
+        coords = get_coord_osm(user.location)
         params = {
             "name": user.name,
-            "posts": user.posts,
-            "location": user.location
+            "location": user.location,
+            "latitude": coords[0],
+            "longitude": coords[1]
+
         }
 
         sql_query = text("""
-                         insert into users (name, posts, location)
-                         values (:name, :posts, :location); \
+                         insert into cemeteries (name, location, latitude, longitude)
+                         values (:name,:location, :latitude, :longitude); \
                          """)
 
         with db_connection.connect() as conn:
